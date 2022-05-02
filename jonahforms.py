ce152ce1622fc48a27f77f638f482467cffe0559
hybornconcept <59656633+hybornconcept@users.noreply.github.com>
2022-05-02 08:44:33 +0100

    Add files via upload

--- jonahforms.py
+++ jonahforms.py
@@ -6,7 +6,7 @@ import pandas as pd
 # import plotly.express as px
 # import plotly.graph_objects as go
 import time
-from streamlit_option_menu import option_menu
+# from streamlit_option_menu import option_menu
 
 st.set_page_config(layout="wide", initial_sidebar_state="collapsed")
 
